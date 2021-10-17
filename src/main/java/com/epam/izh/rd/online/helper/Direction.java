b5315cd98b17de95cbc7398e3793ce158e870d85
ranaviira <84rent@gmail.com>
2021-10-17 20:14:46 +0400

    All test passed

--- src/main/java/com/epam/izh/rd/online/helper/Direction.java
+++ src/main/java/com/epam/izh/rd/online/helper/Direction.java
@@ -1,16 +1,6 @@
 package com.epam.izh.rd.online.helper;
 
-/**
- * Параметр сортировки
- */
 public enum Direction {
-
-    /**
-     * Сортировка по возрастанию
-     */
     ASC,
-    /**
-     * Сортировка по убыванию
-     */
     DESC
 }
--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,50 +2,58 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
-import static java.util.Collections.*;
-
-/**
- * Данный класс обязан использовать StreamApi из функционала Java 8. Функциональность должна быть идентична
- * {@link SimpleTextStatisticsAnalyzer}.
- */
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
     @Override
     public int countSumLengthOfWords(String text) {
-        return 0;
+        return getWords(text).stream().map(String::length).reduce(Integer::sum).get();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return 0;
+        return (int) getWords(text).stream().count();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return 0;
+        return (int) getUniqueWords(text).stream().count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return emptyList();
+        return Arrays.stream(text.split("[\\pP\\s]"))
+                .filter(a -> (!(a.equals(""))))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return emptySet();
+        return Arrays.stream(text.split("[\\pP\\s]"))
+                .filter(a -> (!(a.equals(""))))
+                .collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return emptyMap();
+        return getWords(text)
+                .stream()
+                .collect(Collectors.toMap(element -> element, element -> 1, Integer::sum));
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return emptyList();
+        HashMap<Direction, List<String>> hashMapWordInText = new HashMap<>();
+
+        List<String> ascendingWordsList = getWords(text).stream()
+                .sorted((stringOne, stringTwo) -> stringOne.length() - stringTwo.length()).collect(Collectors.toList());
+        hashMapWordInText.put(Direction.ASC, ascendingWordsList);
+
+        List<String> descendingWordsList = getWords(text).stream()
+                .sorted((stringOne, stringTwo) -> stringTwo.length() - stringOne.length()).collect(Collectors.toList());
+        hashMapWordInText.put(Direction.DESC, descendingWordsList);
+
+        return hashMapWordInText.get(direction);
     }
 }
